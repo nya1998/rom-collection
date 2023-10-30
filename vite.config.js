87094eec912b727974b360b837663a2a574a12f9
Nya1998 <ryn.nya1998@gmail.com>
2023-10-30 18:50:27 +0700

    enable obfuscation

--- vite.config.js
+++ vite.config.js
@@ -9,5 +9,37 @@ export default defineConfig({
   },
   plugins: [
     react(),
+    obfuscatorPlugin({
+      apply: "build",
+      include: ["src/pages/*.jsx"],
+      exclude: [/node_modules/],
+      debugger: true,
+      options: {
+        compact: true,
+        disableConsoleOutput: true,
+        debugProtection: true,
+        simplify: true,
+        selfDefending: true,
+        controlFlowFlattening: true,
+        controlFlowFlatteningThreshold: 0.75,
+        identifierNamesGenerator: 'hexadecimal',
+        splitStrings: true,
+        splitStringsChunkLength: 10,
+        stringArray: true,
+        stringArrayCallsTransform: true,
+        stringArrayCallsTransformThreshold: 0.75,
+        stringArrayEncoding: ['base64'],
+        stringArrayIndexShift: true,
+        stringArrayRotate: true,
+        stringArrayShuffle: true,
+        stringArrayWrappersCount: 2,
+        stringArrayWrappersChainedCalls: true,
+        stringArrayWrappersParametersMaxCount: 4,
+        stringArrayWrappersType: 'function',
+        stringArrayThreshold: 0.75,
+        transformObjectKeys: true,
+        log: false,
+      }
+    })
   ],
 })
